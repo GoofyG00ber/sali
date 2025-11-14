dcc58116fe0d0699aa888143915153687942859f
Nuvagus <ii.gregorburglar@gmail.com>
2025-11-14 19:29:05 +0100

    index

--- server/index.js
+++ server/index.js
@@ -751,7 +751,7 @@ app.post('/api/barion/check-payment', async (req, res) => {
     // Call Barion GetPaymentState API from backend - USES GET METHOD
     const posKey = '4926b2ca-633f-420a-b1dc-c2d03e669fdf'
     const barionUrl = `https://api.test.barion.com/v2/Payment/GetPaymentState?POSKey=${posKey}&PaymentId=${paymentId}`
-    
+
     const barionResponse = await fetch(barionUrl, {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' }
