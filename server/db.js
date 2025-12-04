ed932efd2f2d6db9d84234a107587c53475061c0
Nuvagus <ii.gregorburglar@gmail.com>
2025-12-04 16:12:48 +0100

    db conn

--- server/db.js
+++ server/db.js
@@ -2,10 +2,22 @@ import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
+import fs from 'fs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-dotenv.config({ path: join(__dirname, '.env') })
+
+// Try to load .env from server directory or root directory
+const envPathServer = join(__dirname, '.env')
+const envPathRoot = join(__dirname, '../.env')
+
+if (fs.existsSync(envPathServer)) {
+  dotenv.config({ path: envPathServer })
+} else if (fs.existsSync(envPathRoot)) {
+  dotenv.config({ path: envPathRoot })
+} else {
+  console.warn('No .env file found in server or root directory!')
+}
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
